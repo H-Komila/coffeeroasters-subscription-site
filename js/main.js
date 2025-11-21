c0129ea76cdba54f99fac7185a80901d6150ccbb
H - Komila <komilaxibziddinova06@gmail.com>
2025-11-22 01:42:15 +0500

    htm css qoshtim

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@
             navList.classList.remove('active');
         });
     });
+    
+
+//     const hamburger = document.querySelector('.hamburger');
+// const menu = document.querySelector('.navigation__list'); // navList o'rniga menu
+
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('active');
+//     menu.classList.toggle('active'); // navList o'rniga menu
+// });
+
+
+
